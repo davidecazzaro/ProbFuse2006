d87227641e7eded3257720e0bb16560c839e58c2
Davide <davidecazzaro@users.noreply.github.com>
2017-12-16 22:06:34 +0100

    First commit to set up the repo structure

---
+++ combine.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# helper functions to keep code organized
+from lib.basic_retrieval_helpers import *
+
+def main():
+	print(parse_terrier_run(8))
+
+if __name__ == '__main__':
+   main()
---
+++ lib/basic_retrieval_helpers.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# helper functions to combine runs are in this file
+
+def parse_terrier_run(run_number):
+	return run_number

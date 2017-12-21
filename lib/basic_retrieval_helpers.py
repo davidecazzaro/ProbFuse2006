36c7a72c96f789fac9e914070bfc2db19a5884ef
Luca Venir <luca.venir@studenti.unipd.it>
2017-12-21 19:38:00 +0100

    New functions for new scripts
    extract_ground_truth
    evaluate_run

--- lib/basic_retrieval_helpers.py
+++ lib/basic_retrieval_helpers.py
@@ -19,6 +19,9 @@ def check_folders_exist(path):
 		if not os.path.isdir(path+"/run"+str(i)):
 			raise Exception("Missing folder: "+path+"/run"+str(i))
 
+def check_ground_truth_exist(path):
+	if not os.path.exists(path):
+		raise Exception("Expecting the ground truth file in the /input folder. Check README for info.")
 
 # return a list with the paths of the ten .res files
 def get_res_files(path):
@@ -30,6 +33,27 @@ def get_res_files(path):
 		res_files.append( path+"/run"+str(i)+"/"+file_list[0] )
 	return res_files
 
+# returns a dict with a key for each topic, which has a list of dicts with a key for each document ((0/1) relevance)
+def extract_ground_truth(path):
+	gt = {}
+	with open(path) as fp:
+		for line in fp:
+			line = line.strip()
+			# each line has: top_id, q0, doc_id, rel_weight. Therefore:
+			el = line.split(' ')
+
+			if len(el) != 4:
+				raise Exception("Something is wrong in '"+path+"': "+str(len(el))+" elements found, 4 expected: "+line)
+			tid 	= el[0]
+			did 	= el[2]
+			rel_w 	= el[3]
+
+			if not tid in gt:
+				gt[tid] = {}
+
+			gt[tid][did] = rel_w 
+
+	return gt
 
 # return a dict with a key for each topic which contains a list with doc_id and scores
 def parse_res_file(path_to_file):
@@ -57,6 +81,28 @@ def parse_res_file(path_to_file):
 				buckets[topic_id] = [ extracted_tuple ]
 	return buckets
 
+# this function reads a run and, for each document retuned for each topic, searches for its corresponding ground truth
+# and evaluates if the retrieved document is either relevant, not relevant or neither the two options.
+# then, this function writes down the results in an output file.
+def evaluate_run(run_file, ground_truth, output_file):
+
+	print ("Evaluating at "+output_file+"	..................")
+	
+	with open(run_file) as fp:
+		with open(output_file, 'w') as writer:
+			for line in fp:
+				line = line.strip()
+				el = line.split(' ')
+				topic_id 	= el[0]
+				doc_id 		= el[2]
+
+				if not doc_id in ground_truth[topic_id]:
+					relevance = -1 # "i don't know if it's relevant or not"
+				else:
+					relevance = ground_truth[topic_id][doc_id]
+				newline = topic_id+" "+doc_id+" "+str(relevance)
+				
+				writer.write(newline.strip() + "\n"	)
 
 # normalize given scores in 'topic_tuples' which is a list of tuples
 # return the same tuples with the new normalized score
